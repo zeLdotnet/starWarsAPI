3484e2cb81b144286f40e314917a256f83ea46c4
zeL <3DenzeL@protonmail.com>
2019-08-28 19:57:27 -0400

    first commit for Lab #22

---
+++ Lab23_SWAPI/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab23_SWAPI.Models;
+using System.Net.Http;
+
+namespace Lab23_SWAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+
+        public async Task<IActionResult> GetPersonById(int id)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://swapi.co");
+            var response = await client.GetAsync($"/api/people/{id}/");
+            var person = await response.Content.ReadAsAsync<People>();
+            return View("Result", person);
+        }
+
+        public async Task<IActionResult> GetPlanetById(int id)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://swapi.co");
+            var response = await client.GetAsync($"/api/planets/{id}/");
+            var planet = await response.Content.ReadAsAsync<Planet>();
+            return View("PlanetResult", planet);
+        }
+    }
+}
---
+++ Lab23_SWAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab23_SWAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
